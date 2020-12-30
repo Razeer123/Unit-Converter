23b95185a2a3d56c2aa53950460c3a07c0bce812
Michał Derej <contact@michalderej.com>
2020-12-30 01:40:59 +0100

    Initial Commit

---
+++ Unit Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Converter
+//
+//  Created by Michał Derej on 30/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
